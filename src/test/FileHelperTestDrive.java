597c0d0dad3093aada868313450a3aca190a716e
4thcalabash <751188973@qq.com>
2017-04-24 01:55:14 +0800

    确认无误，迁移完成

--- src/test/FileHelperTestDrive.java
+++ src/test/FileHelperTestDrive.java
@@ -6,8 +6,8 @@ import vo.PlayerVo;
 public class FileHelperTestDrive {
 	public static void main(String[] args){
 		FileHelper fh = new FileHelper();
-		PlayerVo read= fh.loadData(0);
-		fh.saveData(read, 2);
+		PlayerVo read= fh.loadData(2);
+		fh.saveData(read, 1);
 	}
 }
 
