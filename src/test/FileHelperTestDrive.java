4a53e09a64efe9ab718ab6f5e512b2651a2d0765
4thcalabash <751188973@qq.com>
2017-04-25 10:47:59 +0800

    a

--- src/test/FileHelperTestDrive.java
+++ src/test/FileHelperTestDrive.java
@@ -10,6 +10,7 @@ public class FileHelperTestDrive {
 		FileHelper fh = new FileHelper();
 		PlayerVo read = fh.loadData(0);
 		fh.saveData(read, 200);
+		
 		for (int o = 0; o < 3; o++) {
 			MissionVo missionVo = fh.loadMission(o);
 			Mission mission = new Mission(missionVo);
