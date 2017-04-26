dbefb4d7767062d343e9bf66f0ce7cddd5f40187
4thcalabash <751188973@qq.com>
2017-04-27 01:53:24 +0800

    nothing

--- src/test/AITest.java
+++ src/test/AITest.java
@@ -1,5 +1,17 @@
 package test;
 
-public class AITest {
+import bll.matrix.Matrix;
+import bll.platform.Battle;
+import bllservice.BattlePlatform;
+import po.DotPo;
+import po.PopPo;
+import util.AI;
 
+import java.util.Scanner;
+public class AITest {
+	static Scanner reader = new Scanner (System.in);
+	public static void main(String args[]){
+		
+	}
+	
 }
